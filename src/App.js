08af3644dcdbf04ad28e2462e7f3ed8e73c33947
Jordan Renaud <jordan.dj.renaud@gmail.com>
2021-06-14 15:47:20 +0100

    basic styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="background">
+        <div className="circle">
+          <div className="stitches">
+            <h1>Stitch Itch</h1>
+            <h2>It's like tinder for cross stitch.</h2>
+            <h3>You know you want to scratch</h3>
+            <p>Register your interest now:</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
